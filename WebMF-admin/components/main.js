e72a0171a27cea159e845584c6275ae17ec11edf
Adam Ringhede <adamringhede@live.com>
2013-06-26 09:41:03 +0200

    updates monitorviews

--- WebMF-admin/components/main.js
+++ WebMF-admin/components/main.js
@@ -51,10 +51,13 @@ var GameMonitors = new GameMonitorsCollection;
 var GameMonitorView = Backbone.View.extend({
 	tagName: 'li',
 	template: _.template($('#item-template').html()),
+	initialize: function(){
+		this.model.on('change', this.render, this);
+	},
 	render: function() {
 	      this.$el.html(this.template(this.model.toJSON()));
 	      return this;
-	    }
+	}
 });
 
 ///
